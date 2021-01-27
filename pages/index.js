d3a9efd13447e1b0c315ffc1dc7a977cb808a3f4
Rafael Penna <rafaelgpenna@gmail.com>
2021-01-27 11:07:35 -0300

    feat: created route to the quiz page

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,9 @@
 import React from 'react';
 import styled from 'styled-components';
+import { useRouter } from 'next/router';
+
 import db from '../db.json';
+
 import Widget from '../src/components/Widget';
 import QuizLogo from '../src/components/QuizLogo';
 import QuizBackground from '../src/components/QuizBackground';
@@ -19,6 +22,18 @@ export const QuizContainer = styled.div`
 `;
 
 export default function Home() {
+  const router = useRouter();
+  const [name, setName] = React.useState('');
+
+  const startQuiz = (event) => {
+    event.preventDefault();
+    router.push(`/quiz?name=${name}`);
+  };
+
+  const changeName = (event) => {
+    setName(event.target.value);
+  };
+
   return (
     <QuizBackground backgroundImage={db.bg}>
       <QuizContainer>
@@ -28,7 +43,17 @@ export default function Home() {
             <h1>{db.title}</h1>
           </Widget.Header>
           <Widget.Content>
-            <p>{db.description}</p>
+            <form onSubmit={startQuiz}
+            >
+              <input
+                onChange={changeName}
+                placeholder="Tricolor, digite seu nome"
+              />
+              <button type="submit" disabled={name.length === 0}>
+                Jogar
+              </button>
+
+            </form>
           </Widget.Content>
         </Widget>
 
---
+++ pages/quiz.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Quiz = () => (
+  <div>
+    Página do quiz
+  </div>
+);
+
+export default Quiz;
